7325c4ff66196b2b99b7839bf0d08d3d20e985a4
Sachin Jadhav <sjd3333@umd.edu>
2024-10-17 16:26:34 -0400

    Added pipelinefor data preprocessing

--- utils/data_creation.py
+++ utils/data_creation.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta
 import matplotlib.pyplot as plot
 import matplotlib.dates as md
 from scipy import stats
+from pyspark.sql import functions as F
 
 import findspark
 findspark.init()
@@ -44,19 +45,19 @@ def load_data(index, folder_path, part):
     combined_df = combined_df.withColumn("label", lit(index))
     return combined_df
 
-def create_dataset(self, start_time, end_time, cols):
-    timestamps = pd.date_range(start_time, end_time, freq=str(self.granularity)+'ms')
-    data_table = pd.DataFrame(index=timestamps, columns=cols)
-    for col in cols:
-        data_table[str(col)] = np.nan #initialize the columns
+def create_dataset(start_time, end_time, cols, granularity=1000):
+    timestamps = pd.date_range(start_time, end_time, freq=str(granularity)+'ms')
+    data_table = spark.createDataFrame(index=timestamps, columns=cols)
+    for col in cols[1:]:
+        dataframe = dataframe.withColumn(col, F.lit(None))
     return data_table
 
-def num_sampling(self, dataset, data_table, value_cols, aggregation='avg'):
-    for i in range(0, len(data_table.index)):
+def num_sampling(dataset, data_table, value_cols, aggregation='avg', granularity=1000):
+    for i in range(data_table.count()):
         relevant_rows = dataset[
-                (dataset['TimeStamp'] >= data_table.index[i]) &
-                (dataset['TimeStamp'] < (data_table.index[i] +
-                                        timedelta(milliseconds=self.granularity)))]  
+            (dataset['TimeStamp'] >= data_table.select("TimeStamp").head(1)[0][0]) &
+            (dataset['TimeStamp'] < (data_table.select("TimeStamp").head(1)[0][0] +
+                                    timedelta(milliseconds=1000)))] 
         for col in value_cols:
             # numerical cols which for the EEG data are the brain waves
             # We take the average value
